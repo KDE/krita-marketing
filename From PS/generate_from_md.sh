3bdcc8daccfc6e47f737162d6b60bda8948fa13a
Wolthera van Hövell <griffinvalley@gmail.com>
2015-08-06 13:10:03 +0200

    Make the obvious changes to the PS document, add some images, add odt generation
    The look of the resultant PDF files is kina bad, but still.

--- From PS/generate_from_md.sh
+++ From PS/generate_from_md.sh
@@ -2,6 +2,7 @@
 
 #This generates an epub file from the markdown file. Make sure to put spaces between paragraphs and the titles following them!
 pandoc -f markdown -t epub IntroductiontoKritaforusercomingfromPs-4.md -o IntroductiontoKritaforusercomingfromPs-4.epub
+pandoc -f markdown -t odt IntroductiontoKritaforusercomingfromPs-4.md -o IntroductiontoKritaforusercomingfromPs-4.odt
 
 #pandoc -f markdown -t html IntroductiontoKritaforusercomingfromPs-4.md -o IntroductiontoKritaforusercomingfromPs-4.html
 
