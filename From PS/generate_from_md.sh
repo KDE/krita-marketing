933b522619d4391268a117e8102720b91ac68d8f
Wolthera van Hövell <griffinvalley@gmail.com>
2015-08-06 17:24:03 +0200

    Set up and generate styled PDF.
    This'll require a bunch of packages, check the script for more information.

--- From PS/generate_from_md.sh
+++ From PS/generate_from_md.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Generates the titlepage from a kra.
+#krita introduction_title.kra --export --export-filename introduction_title.png
 #This generates an epub file from the markdown file. Make sure to put spaces between paragraphs and the titles following them!
 pandoc -f markdown -t epub IntroductiontoKritaforusercomingfromPs-4.md -o IntroductiontoKritaforusercomingfromPs-4.epub
 pandoc -f markdown -t odt IntroductiontoKritaforusercomingfromPs-4.md -o IntroductiontoKritaforusercomingfromPs-4.odt
@@ -8,4 +10,11 @@ pandoc -f markdown -t odt IntroductiontoKritaforusercomingfromPs-4.md -o Introdu
 
 #From psd, requires tex-live (sudo apt-get install texlive) and lmodern (sudo apt-get install lmodern)
 #This doesn't like special characters like '→'.
-pandoc -f markdown IntroductiontoKritaforusercomingfromPs-4.md -o IntroductiontoKritaforusercomingfromPs-4.pdf
+pandoc -f markdown IntroductiontoKritaforusercomingfromPs-4.md -o IntroductiontoKritaforusercomingfromPs-4.pdf 
+
+#generates a styled PDF use the krita-style.tex.
+#Requires
+#xetex(sudo apt-get install texlive-xetex)
+#rexlive extra(sudo apt-get install texlive-latex-extra)
+#latex-xcolor(sudo apt-get install latex-xcolor)
+pandoc -f markdown IntroductiontoKritaforusercomingfromPs-4.md -o IntroductiontoKritaforusercomingfromPs-4.pdf --latex-engine=xelatex --template krita-style.tex
